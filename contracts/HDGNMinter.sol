b6c7acaaffae63750bb38e829626903c5e2daca9
Literally Collective <bluedragon.cstar@outlook.com>
2021-11-29 00:46:39 -0300

    Feat: publish contracts

---
+++ contracts/HDGNMinter.sol
@@ -0,0 +1,157 @@
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+// HDGN Minter contract.
+contract HDGNMinter is Ownable, ReentrancyGuard {
+    // The HDGN TOKEN!
+    IERC20 public hdgn;
+
+    // The operator can only withdraw wrong tokens in the contract
+    address private _operator;
+
+    // Event
+    event OperatorTransferred(
+        address indexed previousOperator,
+        address indexed newOperator
+    );
+    event OperatorTokenRecovery(address tokenRecovered, uint256 amount);
+
+    modifier onlyOperator() {
+        require(
+            _operator == msg.sender,
+            "operator: caller is not the operator"
+        );
+        _;
+    }
+
+    constructor(IERC20 _hdgn) public {
+        hdgn = _hdgn;
+        _operator = _msgSender();
+
+        emit OperatorTransferred(address(0), _operator);
+    }
+
+    // Safe HDGN transfer function, just in case if rounding error causes pool to not have enough HDGNs.
+    function safeHDGNTransfer(address _to, uint256 _amount)
+        public
+        onlyOwner
+        nonReentrant
+    {
+        uint256 hdgnBal = hdgn.balanceOf(address(this));
+        if (_amount > hdgnBal) {
+            hdgn.transfer(_to, hdgnBal);
+        } else {
+            hdgn.transfer(_to, _amount);
+        }
+    }
+
+    /**
+     * @dev Returns the address of the current operator.
+     */
+    function operator() public view returns (address) {
+        return _operator;
+    }
+
+    /**
+     * @dev Transfers operator of the contract to a new account (`newOperator`).
+     * Can only be called by the current operator.
+     */
+    function transferOperator(address newOperator) external onlyOperator {
+        require(
+            newOperator != address(0),
+            "HDGNMinter::transferOperator: new operator is the zero address"
+        );
+        emit OperatorTransferred(_operator, newOperator);
+        _operator = newOperator;
+    }
+
+    /**
+     * @notice It allows the operator to recover wrong tokens sent to the contract
+     * @param _tokenAddress: the address of the token to withdraw
+     * @param _tokenAmount: the number of tokens to withdraw
+     * @dev This function is only callable by operator.
+     */
+    function recoverWrongTokens(address _tokenAddress, uint256 _tokenAmount)
+        external
+        onlyOperator
+    {
+        IERC20(_tokenAddress).transfer(msg.sender, _tokenAmount);
+        emit OperatorTokenRecovery(_tokenAddress, _tokenAmount);
+    }
+}
